ed3eab1d96df7d68cf964ec5eef2a633ec39b2c8
pawan_kapakayala <pawankapakayala@gmail.com>
2025-02-06 11:06:48 +0530

    company without static

--- company_sir.cpp
+++ company_sir.cpp
@@ -1,28 +1,28 @@
-#include<iostream>
-using namespace std;
+#include<iostream>//io stram is a library function that is used to take input and output
+using namespace std;//std has the standard libraries of c++
 int Capital=50;//global variable
-class companyX{
+class companyX{//class is defined
 	
 	int Capital=50;//local variable
 	int RAmount;
 	public:
-	void Require(int x){
+	void Require(int x){//function is defined
 		RAmount = x;	
 	}	
-	int Allocate(){
+	int Allocate(){//function is defined
 		Capital=Capital-RAmount;
 		return Capital;
 	}
 		
 };
 int main(){
-	companyX A,B,C;
+	companyX A,B,C;//objects are created
 	A.Require(15);
-	int BalA=A.Allocate();
+	int BalA=A.Allocate();//function is called
 	B.Require(10);
-	int BalB=B.Allocate();
+	int BalB=B.Allocate();//function is called
 	C.Require(9);
-	int BalC=C.Allocate();
+	int BalC=C.Allocate();//function is called
 	cout<<BalA<<endl;
 	cout<<BalB<<endl;
 	cout<<BalC<<endl;
