3e6d0d259a12535d23ddd88bac47988ea894ab0e
pawan_kapakayala <pawankapakayala@gmail.com>
2025-03-03 13:54:51 +0530

    Abstract_class

---
+++ abstract_class.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+class a{//class a has a function declared but not defined so it is an abstract class
+    public:
+        void display1();//here we are declaring the function so the class is abstract//this function must be overridden in the child class
+};
+
+class b : public a{
+    public:
+        void display1(){//method overriding - what r the fuctions in the parent class, the same functions are in the child class
+            //this functions overrides the function of the parent class
+            cout<<"I belong to child class"<<endl;
+        }
+};
+
+int main(){
+    b obj;
+    obj.display1();
+    obj.display1(); //this will call the function of the child class
+}
